52655b7d71302d6c235e71e9cd50c2e8046145ab
synergy2411 <synergy2411@outlook.com>
2019-07-15 18:09:56 +0530

    day 01 commit

--- src/app/users/user-info/user-info.component.ts
+++ src/app/users/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent implements OnInit {
+
+  @Input('user') user : any;
 
   constructor() { }
 
