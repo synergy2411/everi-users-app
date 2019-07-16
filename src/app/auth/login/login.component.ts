536022ae87422408ca5be7db60da5aa6489f2816
synergy2411 <synergy2411@outlook.com>
2019-07-16 15:59:07 +0530

    template form added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+
+  onLogin(f: NgForm) {
+    console.log("Username :: ", f.value.username);
+    console.log("Password :: ", f.value.password);
+  }
 
   constructor() { }
 
